02dd91e5cdc18ec49640f7e905410f69cec10782
Adriana Pineda <am.pineda206@uniandes.edu.co>
2016-07-11 20:54:22 -0500

    Add routes to project

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"html"
+	"net/http"
+)
 
 func main() {
-    fmt.Printf("hello, world\n")
+
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/users/{userId}/accounts", GetAccounts).Methods("GET")
+	router.HandleFunc("/accounts", AddAccount).Methods("POST")
+	router.HandleFunc("/accounts", UpdateAccount).Methods("PUT")
+	router.HandleFunc("/accounts", RemoveAccount).Methods("DELETE")
+
+}
+
+func GetAccounts(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Get accounts %q", html.EscapeString(r.URL.Path))
+}
+
+func AddAccount(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Add account %q", html.EscapeString(r.URL.Path))
+}
+
+func UpdateAccount(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Update account %q", html.EscapeString(r.URL.Path))
+}
+
+func RemoveAccount(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Remove account %q", html.EscapeString(r.URL.Path))
 }
