ea05d9f56a5502ef991dd431f039327cafeb92af
Adriana Pineda <am.pineda206@uniandes.edu.co>
2016-07-16 22:13:31 -0500

    Comment code generating warnings

--- handlers.go
+++ handlers.go
@@ -15,8 +15,8 @@ func GetAccounts(w http.ResponseWriter, r *http.Request) {
 	account_2 := Account{Username: "test-username-2", Password: "test-password-2", Url: "test-url-2"}
 	accounts := Accounts{account_1, account_2}
 
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
+	//w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	//w.WriteHeader(http.StatusOK)
 
 	if err := json.NewEncoder(w).Encode(accounts); err != nil {
 		panic(err)
