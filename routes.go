bb5ee11125ca449cdb2c3d317d74535c43422fd7
Adriana Pineda <am.pineda206@uniandes.edu.co>
2016-07-16 22:03:04 -0500

    Split routes.go file into 2

--- routes.go
+++ routes.go
@@ -14,25 +14,6 @@ type Route struct {
 
 type Routes []Route
 
-func NewRouter() *mux.Router {
-	router := mux.NewRouter().StrictSlash(true)
-	for _, route := range routes {
-
-		var handler http.Handler
-
-		handler = route.HandlerFunc
-		handler = Logger(handler, route.Name)
-
-		router.
-			Methods(route.Method).
-			Path(route.Pattern).
-			Name(route.Name).
-			Handler(handler)
-	}
-
-	return router
-}
-
 var routes = Routes{
 	Route{Name: "Get accounts", Method: "GET", Pattern: "/users/{userId}/accounts", HandlerFunc: GetAccounts},
 	Route{Name: "Add Account", Method: "POST", Pattern: "/users/{userId}/accounts", HandlerFunc: AddAccount},
