cf1bec41c3c155c55528f950d7f91676a7e91b82
Adriana Pineda <am.pineda206@uniandes.edu.co>
2016-07-16 21:30:33 -0500

    Add routes.go to refactor code

---
+++ routes.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+type Route struct {
+	Name        string
+	Method      string
+	Pattern     string
+	HandlerFunc http.HandlerFunc
+}
+
+type Routes []Route
+
+func newRouter() *mux.Router {
+	router := mux.NewRouter().StrictSlash(true)
+	for _, route := range routes {
+		router.
+			Methods(route.Method).
+			Path(route.Pattern).
+			Name(route.Name).
+			Handler(route.HandlerFunc)
+	}
+
+	return router
+}
+
+var routes = Routes{
+	Route{Name: "Get accounts", Method: "GET", Pattern: "/users/{userId}/accounts", HandlerFunc: GetAccounts},
+	Route{Name: "Add Account", Method: "POST", Pattern: "/users/{userId}/accounts", HandlerFunc: AddAccount},
+	Route{Name: "Update Account", Method: "PUT", Pattern: "/users/{userId}/accounts", HandlerFunc: UpdateAccount},
+	Route{Name: "Remove Account", Method: "DELETE", Pattern: "/users/{userId}/accounts", HandlerFunc: RemoveAccount},
+}
