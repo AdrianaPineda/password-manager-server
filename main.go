c3cb2231e9ab4b2ee40a97a8da84a46b3ecc1da0
Adriana Pineda <am.pineda206@uniandes.edu.co>
2016-07-16 21:33:44 -0500

    Update main.go file

--- main.go
+++ main.go
@@ -1,46 +1,16 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"github.com/gorilla/mux"
-	"html"
 	"log"
 	"net/http"
 )
 
 func main() {
 
-	router := mux.NewRouter().StrictSlash(true)
-
-	router.HandleFunc("/users/{userId}/accounts", GetAccounts).Methods("GET")
-	router.HandleFunc("/users/{userId}/accounts", AddAccount).Methods("POST")
-	router.HandleFunc("/users/{userId}/accounts", UpdateAccount).Methods("PUT")
-	router.HandleFunc("/users/{userId}/accounts", RemoveAccount).Methods("DELETE")
+	router := NewRouter()
 
 	fmt.Printf("API initialized\n")
 	log.Fatal(http.ListenAndServe(":8080", router))
 
-}
-
-func GetAccounts(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Get accounts %q\n", html.EscapeString(r.URL.Path))
-
-	account_1 := Account{Username: "test-username-1", Password: "test-password-1", Url: "test-url-1"}
-	account_2 := Account{Username: "test-username-2", Password: "test-password-2", Url: "test-url-2"}
-	accounts := Accounts{account_1, account_2}
-
-	json.NewEncoder(w).Encode(accounts)
-}
-
-func AddAccount(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Add account %q\n", html.EscapeString(r.URL.Path))
-}
-
-func UpdateAccount(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Update account %q\n", html.EscapeString(r.URL.Path))
-}
-
-func RemoveAccount(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Remove account %q\n", html.EscapeString(r.URL.Path))
 }
