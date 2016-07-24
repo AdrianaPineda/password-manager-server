3a3e3882d97575a6e08be6430b3bd302693e7220
Adriana Pineda <am.pineda206@uniandes.edu.co>
2016-07-23 23:13:40 -0500

    Add file to manage persistence for accounts model

---
+++ account/persistence.go
@@ -0,0 +1,39 @@
+package account
+
+import (
+	"database/sql"
+	"log"
+)
+
+func GetAccounts(db *sql.DB, userId int) (Accounts, error) {
+
+	rows, err := db.Query("select * from accounts where user_id = ?", userId)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	var currentAccounts Accounts
+
+	var (
+		id       int
+		username string
+		password string
+		url      string
+	)
+	for rows.Next() {
+		err := rows.Scan(&id, &username, &password, &url)
+		if err != nil {
+			log.Fatal(err)
+		}
+		currentAccount := Account{Id: id, Username: username, Password: password, Url: url}
+		currentAccounts = append(currentAccounts, currentAccount)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return currentAccounts, err
+}
