e2de1162e260fedbc93c940b3749e1ab7cadf3dc
Adriana Pineda <am.pineda206@uniandes.edu.co>
2016-10-09 10:10:05 -0500

    Update main file to initialize routes correctly

--- main.go
+++ main.go
@@ -1,19 +1,63 @@
 package main
 
 import (
+	"database/sql"
+	"errors"
 	"fmt"
-	database "github.com/AdrianaPineda/password-manager-server/database"
+	account "github.com/AdrianaPineda/password-manager-server/account"
+	config "github.com/AdrianaPineda/password-manager-server/config"
+	user "github.com/AdrianaPineda/password-manager-server/user"
 	"log"
 	"net/http"
 )
 
+var accountAPI account.AccountAPI
+var userAPI user.UserAPI
+
 func main() {
 
-	database, err := database.InitDB("user=adrianaPineda dbname = adrianaPineda sslmode=disable")
+	database, err := config.InitDB("user=adrianaPineda dbname = adrianaPineda sslmode=disable")
 
-	router := NewRouter()
+	if err != nil {
+		panic(err)
+	}
+
+	accountAPI := createAccountAPI(database)
+	userAPI := createUserAPI(database)
+
+	accountRoutes := getAccountRoutes(accountAPI)
+	userRoutes := getUserRoutes(userAPI)
+
+	routes := append(accountRoutes, userRoutes...)
+
+	router := NewRouter(routes)
 
 	fmt.Printf("API initialized\n")
 	log.Fatal(http.ListenAndServe(":8080", router))
 
+}
+
+func createAccountAPI(database *sql.DB) (accountAPI account.AccountAPI) {
+
+	accountDAO := account.AccountDAO{}
+	accountBusiness := account.AccountBusiness{AccountDAO: accountDAO, Database: database}
+	accountAPI = account.AccountAPI{AccountBusiness: accountBusiness}
+
+	if accountAPI == (account.AccountAPI{}) {
+		panic(errors.New("API couldnt not be initialized"))
+	}
+
+	return accountAPI
+}
+
+func createUserAPI(database *sql.DB) (userAPI user.UserAPI) {
+	userDAO := user.UserDAO{}
+	userBusiness := user.UserBusiness{UserDAO: userDAO, Database: database}
+	userAPI = user.UserAPI{UserBusiness: userBusiness}
+
+	if userAPI == (user.UserAPI{}) {
+		panic(errors.New("API couldnt not be initialized"))
+	}
+
+	return userAPI
 }
