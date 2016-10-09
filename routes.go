72bc9e8777383d547a2d444ab43929ea730501a6
Adriana Pineda <am.pineda206@uniandes.edu.co>
2016-10-09 10:09:52 -0500

    Refactor routes files

--- routes.go
+++ routes.go
@@ -15,15 +15,28 @@ type Route struct {
 
 type Routes []Route
 
-var routes = Routes{
-	Route{Name: "Create user", Method: "POST", Pattern: "/users", HandlerFunc: user.CreateUser},
-	Route{Name: "Get user", Method: "GET", Pattern: "/users/{userId:[0-9]+}", HandlerFunc: user.GetUser},
-	Route{Name: "Get users", Method: "GET", Pattern: "/users", HandlerFunc: user.GetUsers},
-	Route{Name: "Update user", Method: "PUT", Pattern: "/users/{userId:[0-9]+}", HandlerFunc: user.UpdateUser},
-	Route{Name: "Delete user", Method: "DELETE", Pattern: "/users/{userId:[0-9]+}", HandlerFunc: user.DeleteUser},
+func getUserRoutes(userAPI user.UserAPI) Routes {
 
-	Route{Name: "Get accounts", Method: "GET", Pattern: "/users/{userId:[0-9]+}/accounts", HandlerFunc: account.GetAccounts},
-	Route{Name: "Add Account", Method: "POST", Pattern: "/users/{userId:[0-9]+}/accounts", HandlerFunc: account.CreateAccount},
-	Route{Name: "Update Account", Method: "PUT", Pattern: "/users/{userId:[0-9]+}/accounts/{accountId:[0-9]+}", HandlerFunc: account.UpdateAccount},
-	Route{Name: "Delete Account", Method: "DELETE", Pattern: "/users/{userId:[0-9]+}/accounts/{accountId:[0-9]+}", HandlerFunc: account.DeleteAccount},
+	userRoutes := Routes{
+		Route{Name: "Create user", Method: "POST", Pattern: "/users", HandlerFunc: userAPI.CreateUser},
+		Route{Name: "Get user", Method: "GET", Pattern: "/users/{userId:[0-9]+}", HandlerFunc: userAPI.GetUser},
+		Route{Name: "Get users", Method: "GET", Pattern: "/users", HandlerFunc: userAPI.GetUsers},
+		Route{Name: "Update user", Method: "PUT", Pattern: "/users/{userId:[0-9]+}", HandlerFunc: userAPI.UpdateUser},
+		Route{Name: "Delete user", Method: "DELETE", Pattern: "/users/{userId:[0-9]+}", HandlerFunc: userAPI.DeleteUser},
+	}
+
+	return userRoutes
+}
+
+func getAccountRoutes(accountAPI account.AccountAPI) Routes {
+
+	accountRoutes := Routes{
+
+		Route{Name: "Get accounts", Method: "GET", Pattern: "/users/{userId:[0-9]+}/accounts", HandlerFunc: accountAPI.GetAccounts},
+		Route{Name: "Add Account", Method: "POST", Pattern: "/users/{userId:[0-9]+}/accounts", HandlerFunc: accountAPI.CreateAccount},
+		Route{Name: "Update Account", Method: "PUT", Pattern: "/users/{userId:[0-9]+}/accounts/{accountId:[0-9]+}", HandlerFunc: accountAPI.UpdateAccount},
+		Route{Name: "Delete Account", Method: "DELETE", Pattern: "/users/{userId:[0-9]+}/accounts/{accountId:[0-9]+}", HandlerFunc: accountAPI.DeleteAccount},
+	}
+
+	return accountRoutes
 }
